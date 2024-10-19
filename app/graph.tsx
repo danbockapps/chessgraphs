c5a1c8928afa98df5a0b7434bd2a676c0b77edf3
Dan Bock <danbock@gmail.com>
2024-10-19 07:03:44 -0400

    Add recharts

--- app/graph.tsx
+++ app/graph.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {FC, useState} from 'react'
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts'
 import Button from './button'
 
 type Datapoint = {
@@ -13,18 +14,36 @@ type Datapoint = {
 const Graph: FC = () => {
   const [username, setUsername] = useState('')
   const [datapoints, setDatapoints] = useState<Datapoint[]>([])
-  console.log(datapoints)
+
+  const data = datapoints.reduce(
+    (acc, cur) => {
+      const date = new Date(cur.lastMoveAt)
+      const month = date.getMonth()
+      const year = date.getFullYear()
+      const bucket = `${year}/${month + 1}`
+      const category = getReadableTimeControl(cur.clock)
+
+      return {
+        ...acc,
+        [bucket]: {...(acc[bucket] ?? {}), [category]: (acc[bucket]?.[category] ?? 0) + 1},
+      }
+    },
+    {} as Record<string, Record<string, number>>,
+  )
+
+  console.log(data)
 
   const onClick = async () => {
     const onMessage = (obj: Datapoint) => setDatapoints((d) => [...d, obj])
     const onComplete = () => console.log('The stream has completed')
 
-    const oneWeekAgo = new Date()
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 2)
+    const start = new Date()
+    start.setDate(start.getDate() - 30) // It handles month transitions automatically
+    start.setDate(1) // Set it to the beginning of the month
 
     const response = await fetch(
       `https://lichess.org/api/games/user/danbock?${new URLSearchParams({
-        since: `${oneWeekAgo?.getTime() ?? ''}`,
+        since: `${start?.getTime() ?? ''}`,
         moves: 'false',
       })}`,
       {headers: {Accept: 'application/x-ndjson'}},
@@ -58,9 +77,27 @@ const Graph: FC = () => {
     <div>
       <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
       <Button {...{onClick}}>Search</Button>
-      Graph
+      <LineChart
+        width={500}
+        height={300}
+        data={Object.entries(data).map(([name, obj]) => ({name, ...obj}))}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="5 + 3" />
+        <Line type="monotone" dataKey="30 + 20" />
+      </LineChart>
     </div>
   )
+}
+
+const getReadableTimeControl = (clock: {initial: number; increment: number}) => {
+  const minutes = Math.floor(clock.initial / 60)
+  return `${minutes} + ${clock.increment}`
 }
 
 export default Graph
