c3321c081ddb4167e97d71c18d6e5175189aac89
Dan Bock <danbock@gmail.com>
2024-10-20 10:13:29 -0400

    Fill in missing values with zeroes

--- app/getDataArray.ts
+++ app/getDataArray.ts
@@ -19,15 +19,48 @@ const getDataArray = (datapoints: Datapoint[]) => {
     {} as Record<string, Record<string, number>>,
   )
 
-  return Object.entries(dataObj)
-    .map(([month, obj]) => ({month, ...obj}))
-    .sort((a, b) => (a.month < b.month ? -1 : 1))
+  const categories = Object.values(dataObj).reduce((acc, cur) => {
+    const keys = Object.keys(cur).filter((key) => !acc.includes(key))
+    return [...acc, ...keys]
+  }, [] as string[])
+
+  const allMonths = getAllMonths(dataObj)
+
+  return {
+    categories,
+    dataArray: allMonths
+      .map((month) => ({
+        month,
+        ...categories
+          .map((c) => ({[c]: (dataObj[month] ?? {})[c] ?? 0}))
+          .reduce((acc, cur) => ({...acc, ...cur}), {}),
+      }))
+      .sort((a, b) => (a.month < b.month ? -1 : 1)),
+  }
 }
 
 const getReadableTimeControl = (clock: {initial: number; increment: number}) => {
-  if (!clock.initial) console.log('clock.initial is undefined', {clock})
   const minutes = Math.floor((clock.initial || 0) / 60)
   return `${minutes} + ${clock.increment}`
+}
+
+const getAllMonths = (dataObj: Record<string, Record<string, number>>) => {
+  const months = Object.keys(dataObj).sort()
+  if (months.length === 0) return []
+
+  const [startYear, startMonth] = months[0].split('/').map(Number)
+  const [endYear, endMonth] = months[months.length - 1].split('/').map(Number)
+
+  const allMonths = []
+  for (let year = startYear; year <= endYear; year++) {
+    const start = year === startYear ? startMonth : 1
+    const end = year === endYear ? endMonth : 12
+    for (let month = start; month <= end; month++) {
+      allMonths.push(`${year}/${month < 10 ? '0' : ''}${month}`)
+    }
+  }
+
+  return allMonths
 }
 
 export default getDataArray
